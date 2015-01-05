feb1f3e2760c865d55a78af34dee30006f6c37b1
ayrton04 <ayrton04@gmail.com>
2015-01-05 11:51:32 -0500

    Rebase from 'upstream'

--- test/test_ekf.cpp
+++ test/test_ekf.cpp
@@ -1,13 +1,25 @@
-#include "robot_localization/ekf.h"
-#include "robot_localization/filter_common.h"
-
+#include "robot_localization/ros_filter_types.h"
+#include <limits>
 #include <gtest/gtest.h>
 
 using namespace RobotLocalization;
 
+class RosEkfPassThrough : public RosEkf
+{
+  public:
+    RosEkfPassThrough()
+    {
+    }
+
+    Ekf getFilter()
+    {
+      return filter_;
+    }
+};
+
 TEST (EkfTest, Measurements)
 {
-  Ekf ekf;
+  RosEkfPassThrough ekf;
 
   Eigen::VectorXd measurement(STATE_SIZE);
   for(size_t i = 0; i < STATE_SIZE; ++i)
@@ -24,19 +36,21 @@ TEST (EkfTest, Measurements)
   std::vector<int> updateVector(STATE_SIZE, true);
 
   // Ensure that measurements are being placed in the queue correctly
+  ros::Time time;
+  time.fromSec(1000);
   ekf.enqueueMeasurement("odom0",
                          measurement,
                          measurementCovariance,
                          updateVector,
-                         1000);
+                         std::numeric_limits<double>::max(),
+                         time);
 
   std::map<std::string, Eigen::VectorXd> postUpdateStates;
 
-  ekf.integrateMeasurements(1001,
-                            postUpdateStates);
+  ekf.integrateMeasurements(1001);
 
-  EXPECT_EQ(ekf.getState(), measurement);
-  EXPECT_EQ(ekf.getEstimateErrorCovariance(), measurementCovariance);
+  EXPECT_EQ(ekf.getFilter().getState(), measurement);
+  EXPECT_EQ(ekf.getFilter().getEstimateErrorCovariance(), measurementCovariance);
 
   // Now fuse another measurement and check the output.
   // We know what the filter's state should be when
@@ -46,32 +60,33 @@ TEST (EkfTest, Measurements)
 
   measurement2 *= 2.0;
 
+  time.fromSec(1002);
   ekf.enqueueMeasurement("odom0",
                          measurement2,
                          measurementCovariance,
                          updateVector,
-                         1002);
+                         std::numeric_limits<double>::max(),
+                         time);
 
-  ekf.integrateMeasurements(1003,
-                            postUpdateStates);
+  ekf.integrateMeasurements(1003);
 
-  measurement[0] = -4.5198;
-  measurement[1] = 0.14655;
-  measurement[2] = 9.4514;
-  measurement[3] = -2.8688;
-  measurement[4] = -2.2672;
-  measurement[5] = 0.12861;
-  measurement[6] = 15.481;
-  measurement[7] = 17.517;
-  measurement[8] = 19.587;
-  measurement[9] = 9.8351;
-  measurement[10] = 12.73;
-  measurement[11] = 13.87;
-  measurement[12] = 10.978;
-  measurement[13] = 12.008;
-  measurement[14] = 13.126;
+  measurement[0] = -5.2475;
+  measurement[1] = -0.16244;
+  measurement[2] = 10.349;
+  measurement[3] = -2.9844;
+  measurement[4] = -2.3317;
+  measurement[5] = -0.0017943;
+  measurement[6] = 14.875;
+  measurement[7] = 16.848;
+  measurement[8] = 18.822;
+  measurement[9] = 10.077;
+  measurement[10] = 12.907;
+  measurement[11] = 14.246;
+  measurement[12] = 10.963;
+  measurement[13] = 11.991;
+  measurement[14] = 13.276;
 
-  measurement = measurement.eval() - ekf.getState();
+  measurement = measurement.eval() - ekf.getFilter().getState();
 
   for(size_t i = 0; i < STATE_SIZE; ++i)
   {
@@ -81,6 +96,8 @@ TEST (EkfTest, Measurements)
 
 int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "ekf");
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
