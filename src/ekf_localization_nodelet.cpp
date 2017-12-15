a42b5b18e98d09ba736c0e83097a7672d8bfc6c9
Tom Moore <ayrton04@gmail.com>
2017-12-15 13:22:57 +0000

    Rebase from 'release/lunar/robot_localization'

--- src/ekf_localization_nodelet.cpp
+++ src/ekf_localization_nodelet.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015, 2016, Charles River Analytics, Inc.
+ * Copyright (c) 2017 Simon Gene Gottlieb
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,22 +30,33 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "robot_localization/navsat_transform.h"
+#include "robot_localization/ros_filter_types.h"
 
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
 
-int main(int argc, char **argv)
+namespace RobotLocalization
 {
-  ros::init(argc, argv, "navsat_transform_node");
 
-  ros::NodeHandle nh;
-  ros::NodeHandle nh_priv("~");
+class EkfNodelet : public nodelet::Nodelet
+{
+private:
+  std::auto_ptr<RosEkf> ekf;
 
+public:
+  virtual void onInit()
+  {
+    NODELET_DEBUG("Initializing nodelet...");
 
-  RobotLocalization::NavSatTransform trans(nh, nh_priv);
-  ros::spin();
+    ros::NodeHandle nh      = getNodeHandle();
+    ros::NodeHandle nh_priv = getPrivateNodeHandle();
 
-  return EXIT_SUCCESS;
-}
+    ekf.reset(new RosEkf(nh, nh_priv));
+    ekf->initialize();
+  }
+};
 
+}  // namespace RobotLocalization
 
+PLUGINLIB_EXPORT_CLASS(RobotLocalization::EkfNodelet, nodelet::Nodelet);
